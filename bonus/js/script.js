468939c799f1cd202da357989c0086550bbbdb82
Brandon Ladson <brandonladson1999@gmail.com>
2020-11-04 16:57:17 +0100

    creazione regole css e js del file bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
     $(this).children(".dropdown").toggleClass('dbloc');
   });
 
+  $('a > i').click(function () {
+    $('.hamburger-menu').addClass("active");
+  });
+
+  $('a.close > i').click(function () {
+    $('.hamburger-menu').removeClass("active");
+  });
+
 });
